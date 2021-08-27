95fc81e97ce75abd6d6d8956a456626fb4ccd07a
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-08-27 10:17:41 +0300

    Changes for production environment

--- src/main/java/projekti/DatabaseRestController.java
+++ src/main/java/projekti/DatabaseRestController.java
@@ -3,6 +3,7 @@ package projekti;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author tspaanan
  */
+@Profile("development")
 @RestController
 public class DatabaseRestController {
     
--- src/main/java/projekti/ProductionSecurityConfiguration.java
+++ src/main/java/projekti/ProductionSecurityConfiguration.java
@@ -1,23 +1,43 @@
 package projekti;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @Profile("production")
 public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+    
     @Override
-    public void configure(HttpSecurity sec) throws Exception {
-        sec.authorizeRequests()
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/").permitAll()
+                .antMatchers("/javascript/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/search").permitAll()
                 .anyRequest().authenticated().and()
                 .formLogin().permitAll().and()
                 .logout().permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder());
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
