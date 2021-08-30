35b6b2fb9fffb20cade3fca4591148f229ec3e9a
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-08-30 16:07:24 +0300

    New PostgreSQLDialectCustom to fix PostgreSQL error

--- src/main/java/projekti/PostgreSQLDialectCustom.java
+++ src/main/java/projekti/PostgreSQLDialectCustom.java
@@ -3,11 +3,13 @@ package projekti;
 import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
+import org.springframework.context.annotation.Profile;
 
 /**
  *
  * @author tspaanan
  */
+@Profile("production")
 public class PostgreSQLDialectCustom extends PostgreSQL82Dialect {
     
     @Override
